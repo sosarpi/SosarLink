f5bcde04f488b95888d4ca6cdf1149e9bbfd9ae6
BuildTools <unconfigured@null.spigotmc.org>
2019-05-09 19:05:40 +0200

    Added TCP Client

--- app/src/main/java/be/kuleuven/ee5/eliasstalpaert/sosarlink/MainActivity.java
+++ app/src/main/java/be/kuleuven/ee5/eliasstalpaert/sosarlink/MainActivity.java
@@ -5,29 +5,53 @@ import android.content.Intent;
 import android.net.DhcpInfo;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.Formatter;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
-import java.math.BigInteger;
-import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button ftpButton;
+    Button ftpButton, tcpButton, messageButton, tcpCloseButton;
+    TcpClient mTcpClient;
+    TextView receivedText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ftpButton = findViewById(R.id.connectFtpButton);
+        tcpButton = findViewById(R.id.tcpButton);
+        messageButton = findViewById(R.id.tcpMessageButton);
+        tcpCloseButton = findViewById(R.id.tcpCloseButton);
+        receivedText = findViewById(R.id.receivedText);
         ftpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 connectToFtp();
+            }
+        });
+        tcpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                connectTcp();
+            }
+        });
+        messageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                messageTcp();
+            }
+        });
+        tcpCloseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeTcp();
             }
         });
     }
@@ -46,6 +70,25 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void connectTcp() {
+        new ConnectTask().execute("");
+    }
+
+    public void messageTcp() {
+        /*
+        if (mTcpClient != null) {
+            char message = '1';
+            mTcpClient.sendMessage(message);
+        }
+        */
+    }
+
+    public void closeTcp() {
+        if (mTcpClient != null) {
+            mTcpClient.stopClient();
+        }
+    }
+
     private String formatIP(int ip) {
         return String.format(
                 "%d.%d.%d.%d",
@@ -55,4 +98,34 @@ public class MainActivity extends AppCompatActivity {
                 (ip >> 24 & 0xff)
         );
     }
+
+    class ConnectTask extends AsyncTask<String, String, TcpClient> {
+
+        @Override
+        protected TcpClient doInBackground(String... message) {
+
+            //we create a TCPClient object
+            mTcpClient = new TcpClient(new TcpClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            }, receivedText);
+            mTcpClient.run();
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            //response received from server
+            Log.d("test", "response " + values[0]);
+            //process server response here....
+
+        }
+    }
 }
+
